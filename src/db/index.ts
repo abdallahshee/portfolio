af36accc808417c77ed8ff072b9b9bc3fef6e2ff
abdallshshee <abdallahshee664@gmail.com>
2026-03-26 23:08:44 +0300

    changing

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,19 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
-import * as schema from "./schema"
+// import { drizzle } from 'drizzle-orm/node-postgres'
+// import * as schema from "./schema"
 
-const connectionString=process.env.DATABASE_URL!
-export const db = drizzle(connectionString, { schema })
+// const connectionString=process.env.DATABASE_URL!
+// export const db = drizzle(connectionString, { schema })
 
 
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import * as schema from './schema' // ✅ import, not export
 
+export * from './schema' // optional (if you want to re-export)
+
+const connectionString = process.env.DATABASE_URL!
+
+const client = postgres(connectionString, { prepare: false })
+
+export const db = drizzle(client, { schema })
 
