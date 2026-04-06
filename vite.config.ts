afea1e001f8739cf57617f08b82be579f18c853a
abdallshshee <abdallahshee664@gmail.com>
2026-04-06 20:22:07 +0300

    added nitro plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
-
+import { nitro } from 'nitro/vite';
 
 export default defineConfig({
     server: {
@@ -16,6 +16,17 @@ export default defineConfig({
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),  // 👈 before viteReact
+    nitro(),
     viteReact(),
   ],
 })
+
+
+// import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+// import { defineConfig } from 'vite';
+// import viteReact from '@vitejs/plugin-react';
+// import { nitro } from 'nitro/vite';
+
+// export default defineConfig({
+//   plugins: [tanstackStart(), nitro(), viteReact()],
+// });
