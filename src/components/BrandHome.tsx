950b2fe5000caad0d3b31506f0eed9f89ec18b9e
abdallshshee <abdallahshee664@gmail.com>
2026-07-16 23:00:25 +0300

    modified the brand

--- src/components/BrandHome.tsx
+++ src/components/BrandHome.tsx
@@ -3,7 +3,7 @@ const BrandHome = () => {
   return (
      <div className="flex flex-col leading-tight ">
         <span className="text-lg font-normal tracking-wide text-center text-white">
-         Full-Stack Developer {" "} {" "}{" "} 🇰🇪
+         Full-Stack Developer {" "} 🇰🇪
         </span>
       </div>
   )
